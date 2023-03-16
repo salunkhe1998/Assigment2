b0ba335ea211c46274d10ab1641f4c5e0b4c9adb
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-16 21:16:46 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AssigmentDay1
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("******** WELCOME Programing Construct Problem  ");
+
+            // TwoIntegerEqualOrNot.CheckEqualOrNot();
+            // EvenOROdd.CheckEvenOrOdd();
+            //AdmissionEligibility.CheckEligiility();
+            //Weekday.CheackWeekday();
+            //PowerOf2.Cheackpower();
+            // SumofSquare.summation();
+            // FactorialofNumber.factorial();
+            // ReverseNumber.CheckReverseNumber();
+           // SumofNaturalNumber.Display();
+            // ArithmaticOPeration.ArithmaticOperation();
+            //SumNaturalNumber.CheckSumNaturalNumber();
+        }
+
+
+    }
+}
