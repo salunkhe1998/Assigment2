ef33fd85782b5cd4966c3720eed8fe075bbd6c26
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-16 21:05:43 +0530

    Update SumofSquare.cs

--- SumofSquare.cs
+++ SumofSquare.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace AssigmentDay2
 {
-    internal class SumofSquar
+   public class SumofNaturalNumber
     {
+        public static void Display()
+        {
+            int j, sum = 0;
+
+            Console.Write("The first 5 natural number are : ");
+            j = 1;
+            while (j <= 5)
+            {
+                sum = sum + j;
+                Console.Write("{0} ", j);
+                j++;
+            }
+            Console.Write("\nThe Sum is : {0}\n", sum);
+        }
     }
 }
