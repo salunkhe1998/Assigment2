74c2fdf35bd8cca8d99bf895a42d0d62dc03167f
Ashwini <salunkheashwini1998@gmail.com>
2023-03-16 17:52:10 +0530

    check number is even or odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentDay2
+{
+    public class EvenOrOdd
+    {
+        public static void CheckEvenOrOdd()
+        {
+            Console.WriteLine("Enter the Number: ");
+
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            if (Num % 2 == 0)
+            {
+                Console.WriteLine($"{Num} is Even Number.");
+            }
+            else
+            {
+                Console.WriteLine($"{Num} is an Odd Number.");
+            }
+        }
+    }
+}
+    }
+}
