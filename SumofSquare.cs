af13dec07aef4fa5f492357ebcf86079951d1fe3
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-16 20:54:24 +0530

    Update SumofSquare.cs

--- SumofSquare.cs
+++ SumofSquare.cs
@@ -8,5 +8,20 @@ namespace AssigmentDay2
 {
     internal class SumofSquar
     {
+        public static void summation()
+        {
+            Console.WriteLine("Enter n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += (i * i);
+            }
+            Console.Write("\nThe Sum of Square Natural Number upto {0} terms = {1} \n", n, sum);
+        }
+
+    }
+}
     }
 }
