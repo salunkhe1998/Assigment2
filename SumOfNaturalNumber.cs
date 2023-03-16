f1fe6cc21a334b63c8327445315fb6c5e662f777
Ashwini <salunkheashwini1998@gmail.com>
2023-03-16 21:00:00 +0530

    cheaking  sum ofntural number number

---
+++ SumOfNaturalNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentDay2
+{
+    public class SumofNaturalNumber
+    {
+        public static void Display()
+        {
+            int j, sum = 0;
+
+            Console.Write("The first 5 natural number are : ");
+            j = 1;
+            while (j <= 5)
+            {
+                sum = sum + j;
+                Console.Write("{0} ", j);
+                j++;
+            }
+            Console.Write("\nThe Sum is : {0}\n", sum);
+        }
+    }
+}
