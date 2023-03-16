50637d66fe9aa852a88c0c7115e6d78a2279446e
Ashwini <salunkheashwini1998@gmail.com>
2023-03-16 17:42:46 +0530

    check equal or not

---
+++ TwoIntegerEqualOrNot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentDay2
+{
+    public class TwoIntegerEqualOrNot
+    {
+        public static void CheckEqualOrNot()
+        {
+
+
+            int num1, num2;
+
+            Console.WriteLine("Input the values for Number1 and Number2 : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Number1 and Number2 are equal\n");
+            }
+
+            else
+            {
+                Console.WriteLine("Number1 and Number2 are not equal\n");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
