aa283a9e41dc3fba0388e957738365ed42ece150
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-17 14:06:23 +0530

    Update FactorialofNumber.cs

--- FactorialofNumber.cs
+++ FactorialofNumber.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace AssigmentDay2
 {
-    internal class FactorialofNumber
-    {
-    }
+    public class FactorialofNumber
+    { 
+            public static void factorial()
+            {
+                int i, f = 1, num;
+
+                Console.Write("Input the number : ");
+                num = Convert.ToInt32(Console.ReadLine());
+                for (i = 1; i <= num; i++)
+                {
+                    f = f * i;
+                }
+                Console.Write("The Factorial of {0} is: {1}\n", num, f);
+            }
+        }
+
 }
