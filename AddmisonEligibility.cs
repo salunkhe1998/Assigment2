51af60216daa96292247154e1d528a898ed6891e
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-17 14:04:36 +0530

    Update AddmisonEligibility.cs

--- AddmisonEligibility.cs
+++ AddmisonEligibility.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace AssigmentDay2
 {
-    internal class AddmisonEligibilitycs
+     public class AdmissionEligibility
     {
+        public static void CheckEligiility()
+        {
+            Console.WriteLine("Enter Math mark");
+            int MM=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Physics mark");
+            int PM = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Chemistry mark");
+            int CM = Convert.ToInt32(Console.ReadLine());
+            int TM = MM + PM + CM;
+            Console.WriteLine("Total Marks are" +  TM);
+
+            if (MM>=65 && PM>=55 && CM>=50 && TM>=180)
+            {
+                Console.WriteLine("Your eligible for taking admission");
+            }
+            else if ((MM+PM)>=140 || (MM+CM)>=140)
+            {
+                Console.WriteLine("Your eligible for taking admission");
+            }
+            else
+            {
+                Console.WriteLine("Your not eligible for taking admission");
+            }
+        }
     }
 }
