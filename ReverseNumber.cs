19aa8c33fbf94d5f61f1a50fbee1fa390cb635b5
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-17 14:10:30 +0530

    Update ReverseNumber.cs

--- ReverseNumber.cs
+++ ReverseNumber.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace AssigmentDay2
 {
-    internal class ReverseNumber
+   public class ReverseNumber
     {
+        public static void CheckReverseNumber()
+        {
+            int Reverse = 0;
+            Console.WriteLine("Enter the number :");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            //while(num>0)
+
+            for (int i = num; num > 0; i++)
+            {
+                int Reminder = num % 10;
+                Reverse = Reverse * 10 + Remainder;
+                num = num / 10;
+            }
+            Console.WriteLine(Reverse);
+
+        }
     }
 }
